f5bc7d4025f4c16a8a397334d4ddd770c10a1584
Newburu <newburu@gmail.com>
2025-12-09 22:33:32 +0900

    feat: Omniauthでのユーザー作成・更新ロジックを改善し、デバッグログを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,27 +9,43 @@ class User < ApplicationRecord
   has_many :reflections, dependent: :destroy
 
   def self.from_omniauth(auth)
+    Rails.logger.info "OMNIAUTH DEBUG: Auth Payload: #{auth.inspect}"
+
     # Check for existing user by provider and uid
     user = find_by(provider: auth.provider, uid: auth.uid)
-    return user if user
+    if user
+      Rails.logger.info "OMNIAUTH DEBUG: Updating existing user #{user.id} with name: #{auth.info.name}"
+      user.update(name: auth.info.name)
+      return user
+    end
 
-    # Check for existing user by email
-    if auth.info.email
-      user = find_by(email: auth.info.email)
+    # Check for existing user by email (if email is provided)
+    email = auth.info.email
+    if email
+      user = find_by(email: email)
       if user
-        # Link the account
+        Rails.logger.info "OMNIAUTH DEBUG: Linking existing user #{user.id} by email"
         user.update(provider: auth.provider, uid: auth.uid, name: auth.info.name)
         return user
       end
     end
 
     # Build a new user
-    new.tap do |new_user|
-      new_user.provider = auth.provider
-      new_user.uid = auth.uid
-      new_user.email = auth.info.email if auth.info.email
-      new_user.name = auth.info.name if auth.info.name
-      new_user.password = Devise.friendly_token[0, 20]
+    Rails.logger.info "OMNIAUTH DEBUG: Creating new user"
+    new_user = new
+    new_user.provider = auth.provider
+    new_user.uid = auth.uid
+    new_user.email = email || "#{auth.uid}-#{auth.provider}@example.com"
+    new_user.name = auth.info.name
+    new_user.password = Devise.friendly_token[0, 20]
+
+    # Save the user to ensure persistence
+    if new_user.save
+      Rails.logger.info "OMNIAUTH DEBUG: Created user #{new_user.id}"
+      new_user
+    else
+      Rails.logger.error "OMNIAUTH DEBUG: Failed to create user: #{new_user.errors.full_messages}"
+      new_user # Return unsaved user so controller can show errors
     end
   end
 end
